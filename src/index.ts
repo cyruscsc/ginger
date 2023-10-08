fb78dc2d4aef0f53f735a43988e39766d9a18cdd
Cyrus Chan <sairuscsc@gmail.com>
2023-10-08 13:37:17 -0400

    create eventing and sync models

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// import { User } from './models/User';
+
+// const user = new User({ name: 'new user', age: 40 });
+
+// user.events.on('change', () => {
+//   console.log('changed!');
+// });
+
+// user.events.trigger('change');
