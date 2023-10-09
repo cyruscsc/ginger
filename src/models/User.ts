bf6bd255a632b86b46a01e1d1f03018ed2f0edf0
Cyrus Chan <sairuscsc@gmail.com>
2023-10-09 14:13:51 -0400

    integrate all methods to into user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
@@ -11,11 +12,45 @@ export interface UserProps {
 }
 
 export class User {
+  public attrs: Attributes<UserProps>;
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
 
   constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
+    this.attrs = new Attributes<UserProps>(attrs);
+  }
+
+  get get() {
+    return this.attrs.get;
+  }
+
+  get getAll() {
+    return this.attrs.getAll;
+  }
+
+  set(update: UserProps): void {
+    this.attrs.set(update);
+    this.events.trigger('change');
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+    if (!id) throw new Error('Cannot fetch without an id');
+    this.sync.fetch(id).then((res: AxiosResponse): void => this.set(res.data));
+  }
+
+  save(): void {
+    this.sync
+      .save(this.getAll())
+      .then((res: AxiosResponse): void => this.trigger('save'))
+      .catch(() => this.trigger('error'));
   }
 }
